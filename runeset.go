3eed3d22a7d2e20b99afe88d566e060a41b540c6
Luciano Ramalho <luciano@ramalho.org>
2018-01-27 00:13:42 -0200

    extracted runeset from old runefinder example

---
+++ runeset.go
@@ -0,0 +1,37 @@
+package runeset
+
+// Set represents a set of runes
+type Set map[rune]struct{}
+
+// MakeFromString creates and returns a new Set
+func MakeFromString(chars string) Set {
+	s := Set{}
+	for _, c := range chars {
+		s.Add(c)
+	}
+	return s
+}
+
+// Add adds a rune to the set.
+func (s Set) Add(r rune) {
+	s[r] = struct{}{} // zero-byte struct
+}
+
+// Has reports whether set contains given rune
+func (s Set) Has(r rune) bool {
+	_, found := s[r]
+	return found
+}
+
+// Intersection returns a new set: the intersection of s and other
+func (s Set) Intersection(other Set) Set {
+	result := Set{}
+	if len(other) > 0 {
+		for r := range s {
+			if other.Has(r) {
+				result.Add(r)
+			}
+		}
+	}
+	return result
+}
---
+++ runeset_test.go
@@ -0,0 +1,103 @@
+package runeset
+
+import (
+	"reflect"
+	"testing"
+)
+
+const (
+	universe  = "0123456789"
+	even      = "02468"
+	odd       = "13579"
+	prime     = "2357"
+	fibonacci = "012358"
+	singleton = "1"
+	void      = ""
+)
+
+var (
+	U = MakeFromString(universe)
+	E = MakeFromString(even)
+	O = MakeFromString(odd)
+	P = MakeFromString(prime)
+	F = MakeFromString(fibonacci)
+	V = MakeFromString(void)
+)
+
+func TestHas_empty(t *testing.T) {
+	var s Set
+	if s.Has('x') {
+		t.Errorf(`s is empty, no words should be found.`)
+	}
+}
+
+func TestHas(t *testing.T) {
+	if !E.Has('2') {
+		t.Errorf(`2 should be in E (even)`)
+	}
+}
+
+func TestAdd_empty(t *testing.T) {
+	s := Set{}
+	r := 'x'
+	s.Add(r)
+	if res := !s.Has(r); res {
+		t.Errorf(`%q was added, but s.Has(%[1]q) == %v.`, r, res)
+	}
+}
+
+func TestMakeFromString(t *testing.T) {
+	chars := "abc"
+	s := MakeFromString(chars)
+	for _, c := range chars {
+		if res := !s.Has(c); res {
+			t.Errorf(`%q was added, but s.Has(%[1]q) == %v.`, c, res)
+		}
+	}
+
+}
+
+func TestIntersection(t *testing.T) {
+	s1 := MakeFromString("abcd")
+	s2 := MakeFromString("bdz")
+	want := MakeFromString("bd")
+	got := s1.Intersection(s2)
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf(`Wanted: %v Got: %v`, want, got)
+	}
+
+}
+
+func TestIntersection_empty(t *testing.T) {
+	s1 := MakeFromString("abcd")
+	s2 := MakeFromString("xyz")
+	want := Set{}
+	got := s1.Intersection(s2)
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf(`Wanted: %v Got: %v`, want, got)
+	}
+
+}
+
+func TestIntersection_table(t *testing.T) {
+	testCases := []struct {
+		name   string
+		first  Set
+		second Set
+		want   Set
+	}{
+		{"even & prime", E, P, MakeFromString("2")},
+		{"even & fibonacci", E, F, MakeFromString("028")},
+		{"even & odd", E, O, V},
+		{"void & universe", V, U, V},
+		{"even & universe", E, U, E},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			got := tc.first.Intersection(tc.second)
+			if !reflect.DeepEqual(tc.want, got) {
+				t.Errorf("got %v; want %v", got, tc.want)
+			}
+		})
+	}
+}
